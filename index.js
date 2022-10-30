2fc67e0b45b4a2c5777267bdd38c24f511602375
20wh1a0561 <20wh1a0561@bvrithyderabad.edu.in>
2022-10-30 11:27:59 +0530

    using ejs and database

--- index.js
+++ index.js
@@ -2,9 +2,60 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+
+var serviceAccount = require("./key.json");
+
+initializeApp({
+    credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
+
+app.set('view engine',"ejs")
+
+
 app.get('/', (req, res) => {
-  res.send('hi vapjxidbc')
+  res.send('hi hello vanakkam')
 })
+
+app.get('/signin', (req, res) => {
+    res.render('signin')
+})
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+app.get('/signinsubmit', (req, res) => {
+    const email = req.query.email;
+    const password = req.query.password;
+    db.collection('users')
+        .where("email","==",email)
+        .where("password","==",password)
+        .get()
+        .then((docs) => {
+            if (docs.size > 0){
+                res.render("home");
+            }
+            else{
+                res.send("Login Failed")
+            }
+        });
+});
+
+app.get('/signupsubmit', (req, res) => {
+    const full_name = req.query.full_name;
+    const email = req.query.email;
+    const password = req.query.password;
+    db.collection('users').add({
+        name:full_name,
+        email:email,
+        password:password,
+    }).then(()=>{
+        res.send("SIGNUP Successful.")
+    });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
