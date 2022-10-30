585fd49e3929285cd7568632f9b1da8b5a5fba1a
20wh1a0561 <20wh1a0561@bvrithyderabad.edu.in>
2022-10-30 22:55:40 +0530

    FINAL COMMIT

--- index.js
+++ index.js
@@ -78,13 +78,19 @@ app.get('/query',function(req,res){
             const img3 = JSON.parse(body).results[3].image;
             const img4 = JSON.parse(body).results[4].image;
             const img5 = JSON.parse(body).results[5].image;
+            const img6 = JSON.parse(body).results[6].image;
+            const img7 = JSON.parse(body).results[7].image;
+            const img8 = JSON.parse(body).results[8].image;
             const title0 = JSON.parse(body).results[0].title;
             const title1 = JSON.parse(body).results[1].title;
             const title2 = JSON.parse(body).results[2].title;
             const title3 = JSON.parse(body).results[3].title;
             const title4 = JSON.parse(body).results[4].title;
             const title5 = JSON.parse(body).results[5].title;
-            res.render('foodrecipe',{q:q,img0:img0,img1:img1,img2:img2,img3:img3,img4:img4,img5:img5,title0:title0,title1:title1,title2:title2,title3:title3,title4:title4,title5:title5})
+            const title6 = JSON.parse(body).results[6].title;
+            const title7 = JSON.parse(body).results[7].title;
+            const title8 = JSON.parse(body).results[8].title;
+            res.render('foodrecipe',{q:q,img0:img0,img1:img1,img2:img2,img3:img3,img4:img4,img5:img5,img6:img6,img7:img7,img8:img8,title0:title0,title1:title1,title2:title2,title3:title3,title4:title4,title5:title5,title6:title6,title7:title7,title8:title8})
         }
         else{
             res.send("smtg went wrong");
